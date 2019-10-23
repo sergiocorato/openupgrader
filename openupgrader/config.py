1424ece622a0027124a58fe88126459eb2af2247
sergiocorato <sergiocorato@gmail.com>
2019-10-23 17:54:43 +0200

    [IMP] config

---
+++ openupgrader/config.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+import kaptan
+
+
+def load_config(config, version):
+    if not os.path.exists(config):
+        local_path = os.path.join(os.path.expanduser('~'), config)
+        if not os.path.exists(local_path):
+            raise Exception('Unable to find configuration file: %s' % config)
+        else:
+            config = local_path
+
+    file_extension = os.path.splitext(config)[1][1:]
+    conf = kaptan.Kaptan(handler=kaptan.HANDLER_EXT.get(file_extension))
+
+    conf.import_config(config)
+    repos = conf.export('dict') or {}
+    res = {}
+    for repo in repos.get('repositories'):
+        if repo.get('version') == version:
+            res = repo.get('remotes')
+    return res
